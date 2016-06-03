f788cf79b2df4fde53e9b312f60bfbd332a8a17c
popze <394462606@qq.com>
2016-06-04 00:18:00 +0800

    测试

--- app/src/main/java/com/jshs/mobile/banmen/HomeActivity.java
+++ app/src/main/java/com/jshs/mobile/banmen/HomeActivity.java
@@ -1,9 +1,10 @@
 package com.jshs.mobile.banmen;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
