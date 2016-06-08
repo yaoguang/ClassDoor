b5e4d22995186cdcbd40828eef788fb69c7cb010
SZH <1760467853@qq.com>
2016-06-08 21:51:31 +0800

    改错了东西，改回去

--- app/src/main/java/com/jshs/mobile/banmen/FunctionPage/MinePager/MineFragment.java
+++ app/src/main/java/com/jshs/mobile/banmen/FunctionPage/MinePager/MineFragment.java
@@ -13,19 +13,19 @@ import com.jshs.mobile.banmen.R;
 /**
  * Created by icezers on 16/6/8.
  */
-public class MineFragment extends BaseHomePager{
-	View _rootView;
+public class MineFragment extends BaseHomePager {
+    View _rootView;
 
-	@Override
-	public void onPagerSelect() {
-		TextView textView= (TextView) _rootView.findViewById(R.id.text);
-		textView.setText(TAG());
-	}
+    @Override
+    public void onPagerSelect() {
+        TextView textView = (TextView) _rootView.findViewById(R.id.text);
+        textView.setText(TAG());
+    }
 
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-		_rootView=inflater.inflate(R.layout.mine_fragment, container, false);
-		return _rootView;
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        _rootView = inflater.inflate(R.layout.my_fragment, container, false);
+        return _rootView;
+    }
 }
